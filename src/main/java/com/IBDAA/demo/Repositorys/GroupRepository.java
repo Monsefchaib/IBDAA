d5d7921c91d8ace04957083f9f4d4b533052ea0b
belhaoudi-oussama <belhaoudi.oussama@gmail.com>
2021-07-13 03:01:46 -0700

    switch from date to date time and remove duration

---
+++ src/main/java/com/IBDAA/demo/Repositorys/GroupRepository.java
@@ -0,0 +1,11 @@
+package com.IBDAA.demo.Repositorys;
+
+import com.IBDAA.demo.Models.Formation;
+import com.IBDAA.demo.Models.Groupe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GroupRepository extends JpaRepository<Groupe,Long> {
+
+}
